1286b044b44642f397faca62949cd3933d630a0a
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2023-08-04 19:37:52 -0300

    3. Criando componente Card para o receber Login

---
+++ my-app-ts/src/components/Card.tsx
@@ -0,0 +1,12 @@
+import {Header}  from "./Header/Header"
+import {Login} from "./Login"
+
+export const Card = ()=>{
+    
+    return(
+        <>
+            <Header/>
+            <Login/>
+        </>
+    )
+}
